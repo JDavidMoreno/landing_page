a34dfcdd0d3c93283956beb90d12dd4700eb17c5
Tiana <tianart22@icloud.com>
2020-03-22 17:56:20 +0100

    all is now like before

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,8 +4,7 @@ import React from "react";
 export default class Footer extends React.Component {
     render() {
         return (
-            <div style={{backgroundColor: 'black', padding: '2.5rem', textAlign: 'center', width: '100%'}}>'
-            aa
+            <div style={{backgroundColor: 'black', padding: '2.5rem', textAlign: 'center', width: '100%'}}>
                 <span style={{color: 'gray'}}>
                     { this.props.content }
                 </span>
